71f97eccedd973894eb76f7775b1f8a26bc2756c
aleixbermudez <aleixbermudez19@gmail.com>
2025-01-14 13:55:34 +0000

    Controlador Playa, creado.

--- routes/web.php
+++ routes/web.php
@@ -10,3 +10,26 @@ Route::get('/playas', function () {
     return view('playas');
 });
 
+Route::get('/usuario/{id}', function ($id) {
+    return 'ID de Usuario: ' . $id;
+});
+
+Route::get('/contacto', function () {
+    $url = route('contacto'); 
+    return 'Página de contacto';
+})->name('contacto');
+
+Route::get('/login', function() {
+    return 'Esto es la página del login!';
+})->name('login');
+
+
+Route::middleware(['auth'])->group(function () {
+    Route::get('/admin/usuarios', function () {
+        return 'Gestión de usuarios';
+    });
+    Route::get('/admin/configuracion', function () {
+        return 'Configuración del sistema';
+    });
+});
+
